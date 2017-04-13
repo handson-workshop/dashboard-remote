ee34b9a22eb1e9cc22f8f1a50f42f9992073d565
Arnaldo Crescente <acr@bit4id.com>
2017-04-13 13:08:46 +0200

    Aggiornta l'interfaccia del canale con la nav bar

--- app/src/dr/core/static/js/client.js
+++ app/src/dr/core/static/js/client.js
@@ -1,17 +1,20 @@
 (function () {
     "use strict";
 
+    var lasturl = "";
+
     var get_cast_url = function() {
         var url = document.body.dataset.url;
-        var frame = document.getElementById('frame');
+        var frame = document.getElementById('stream');
         var req = new XMLHttpRequest();
         req.onreadystatechange = function() { 
-            if (req.readyState == 4 && req.status == 200) {
+            if (req.readyState == XMLHttpRequest.DONE && req.status == 200) {
                 var response = JSON.parse(req.responseText);
                 console.log(response);
-                if(frame.src !== response.url)
+                if(lasturl !== response.url)
                 {
                     console.log('Reload');
+                    lasturl = response.url;
                     frame.src = response.url;
                 }
                 setTimeout(get_cast_url, 5000);
