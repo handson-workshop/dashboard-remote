9deddb9941c2858091707a1474cc1fbd1a6ec420
acr <acr@bit4id>
2017-05-26 16:05:29 +0200

    Correzione bug,  in caso di errore nel recupero della url e incorporato il client HTTP

--- app/src/dr/core/static/js/client.js
+++ app/src/dr/core/static/js/client.js
@@ -1,29 +1,42 @@
-(function () {
+(function (window) {
     "use strict";
 
     var lasturl = "";
+    var frame = document.getElementById('stream');
+    var url = document.body.dataset.url;
+    
+    window.DEBUG = true;
+
+    var get_cast_url_success = function(response)
+    {
+        var response = JSON.parse(response);
+        if(DEBUG) console.log("Get cast url success: ", response);
+        if(lasturl !== response.url)
+        {
+            if(DEBUG) console.log('Update URL');
+            lasturl = response.url;
+            frame.src = response.url;
+        }
+    }
+
+    var get_cast_url_failure = function(response)
+    {
+        alert("Errore imprevisto");
+        if(DEBUG) console.log('Get cast url error: ', response);
+    }
+
+    var get_cast_url_done = function()
+    {
+        if(DEBUG) console.log("Request new url");
+        setTimeout(get_cast_url, 5000);
+    }
 
     var get_cast_url = function() {
-        var url = document.body.dataset.url;
-        var frame = document.getElementById('stream');
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function() { 
-            if (req.readyState == XMLHttpRequest.DONE && req.status == 200) {
-                var response = JSON.parse(req.responseText);
-                console.log(response);
-                if(lasturl !== response.url)
-                {
-                    console.log('Reload');
-                    lasturl = response.url;
-                    frame.src = response.url;
-                }
-                setTimeout(get_cast_url, 5000);
-            }
-        }
-
-        req.open("GET", url, true);
-        req.send( null );
+        httpGetAsync(url, 
+                     get_cast_url_success,
+                     get_cast_url_failure,
+                     get_cast_url_done);
     };
 
     get_cast_url();
-}());
+}(window));
