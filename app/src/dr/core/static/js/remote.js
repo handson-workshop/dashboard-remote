9a91d87883c8345ff81f026ad103edf076afade7
acr <acr@bit4id>
2017-05-26 16:08:22 +0200

    Correzione bug sulla validazione dell'url e compatibilità con Edge e smartphone

--- app/src/dr/core/static/js/remote.js
+++ app/src/dr/core/static/js/remote.js
@@ -7,23 +7,22 @@
 	var castButton = document.getElementById('cast-button');
 	var dimmer = document.querySelector('.dimmer');
 	var channelName = '';
+	var urlField = document.getElementById("url-field");
 	
 	// Gestione dei canali selezionati
-	document.querySelectorAll(".display input").forEach(function (e) {
-		e.addEventListener('change', function (event) {
-			if(this.checked) {
-				channels.push(e.value);
-			}
-			else {
-				channels.splice(channels.indexOf(e.value), 1);
-			}
-			
+	var inputs = document.querySelectorAll(".display input");
+	for(var i = 0; i < inputs.length; i++){
+		inputs[i].addEventListener('change', function (event) {
+			if(this.checked)
+				channels.push(this.value);
+			else
+				channels.splice(channels.indexOf(this.value), 1);
+
 			castButton.disabled = (channels.length === 0) ? true : false;
 		});
-	});
+	}
 
-	document.getElementById("url-field").onkeydown = function (event) {
-		console.log(event.which);
+	urlField.onkeydown = function (event) {
 		if(event.which === 13 && channels.length > 0) {
 			castUpdate();
 		}
@@ -35,49 +34,60 @@
 
 	// Inizializza l'aggiornamento dei canali
 	function castUpdate() {
-		var url = document.getElementById("url-field").value;
-		updateChannels(url, channels);
+		var url = urlField.value;
+		updateChannels(url);
 	}
 
 	// Esegue la POST per l'aggiornamento url del/dei canali selezionati
-	function updateChannels(url, channels) {
-        var req = new XMLHttpRequest();
+	function updateChannels(url) {
 		var data = new FormData();
 
-		if(!validateUrl(url)) {
+		if(document.getElementById("url-field").checkValidity()) {
 			// Setto errore
-
-			return
+			alert("url error");
+			return;
 		}
 
-		if(url.toLowerCase().substr(0, 4) !== 'http') {
-			url = 'http://' + url;
-		}
+		url = composeUrl(url);
 
 		data.append('url', url);
 		data.append('channels', channels.join(","));
-        req.open("POST", 'cast/_update', true);
-		req.setRequestHeader("X-CSRFToken", token);
 
-		req.onreadystatechange = function() {
-			if(req.readyState == XMLHttpRequest.DONE && req.status == 200) {
-				document.getElementById("url-field").value = "";
+		var extra = {};
+		extra.headers = [];
+		extra.data = data;
+		extra.headers["X-CSRFToken"] = token;
+		
+		var updateChannels_success = function() {
+			urlField.value = "";
 
-				for(var i=0; i < channels.length; i++) {
-					var frame = document.getElementById('frame-' + channels[i]);
-					frame.src=url;
-
-					document.getElementById('channel-' + channels[i]).checked = false;
-				}
+			for(var i=0; i < channels.length; i++) {
+				var frame = document.getElementById('frame-' + channels[i]);
+				frame.src=url;
+				document.getElementById('channel-' + channels[i]).checked = false;
 			}
+			channels = [];
+			castButton.disabled = true;
+		}
+		httpPostAsync('cast/_update', updateChannels_success, extra);
+    }
+
+	function composeUrl(url)
+	{
+		if(url.toLowerCase().substr(0, 7) !== 'http://' &&
+		   url.toLowerCase().substr(0, 8) !== 'https://') {
+			url = 'http://' + url;
 		}
 
-        req.send(data);
-    }
+		return url;
+	}
 
 	// TOOLBAR DISPLAY EVENT
 	// Rimozione del canale
-	document.querySelectorAll('.display .remove').forEach(function(item){
+	var display = document.querySelectorAll('.display .remove');
+	
+	for(var i = 0; i < display.length; i++) {
+		var item = display[i];
 		// Recuperare valore in data-value e far apparire il popup
 		item.onclick = function(e) {
 			channelName = this.getAttribute('data-value');
@@ -86,7 +96,7 @@
 			dimmer.className += ' visible';
 			dimmer.querySelector('#remove-channel').className += ' visible';
 		};
-	});
+	}
 	
 	// POPUP EVENT
 	// Conferma rimozione canale
@@ -95,39 +105,33 @@
 		var data = new FormData();
 
 		data.append('channel', channelName);
-        req.open("POST", 'cast/_remove', true);
-		req.setRequestHeader("X-CSRFToken", token);
-
-		req.onreadystatechange = function() { 
-            if (req.readyState == XMLHttpRequest.DONE && req.status == 200) {
-				// Rimozione grafica del canale
-                document.getElementById('display-' + channelName).remove();
-				closePopup('#remove-channel');
-            }
-        }
-
-        req.send(data);
+		
+		var extra = {};
+		extra.headers = [];
+		extra.data = data;
+		extra.headers["X-CSRFToken"] = token;
+		
+		var updateChannels_success = function() {
+			// Rimozione grafica del canale
+			document.getElementById('display-' + channelName).remove();
+			closePopup('#remove-channel');
+		}
+		httpPostAsync('cast/_remove', updateChannels_success, extra);
 	};
 
 	// Chiudi il popup
-	dimmer.querySelectorAll('.dimmer .canc').forEach(function(item) {
+	var closeButtons = dimmer.querySelectorAll('.dimmer .canc');
+	
+	for(var i = 0; i < closeButtons.length; i++) {
+		var item = closeButtons[i];
 		item.onclick = function(e) {
 			closePopup('#remove-channel');
 		};
-	});
+	}
 
 	function closePopup(popupID) {
 		dimmer.classList.remove('visible');
 		dimmer.querySelector(popupID).classList.remove('visible');
-	}
-
-	// VALIDATOR
-	// Validator per un URL
-	function validateUrl(myurl) {
-		var expression = /(https?:\/\/(www\.)?)?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-		var regex = new RegExp(expression);
-
-		return myurl.match(regex);
 	}
 
 }());
